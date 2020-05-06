62234d2e7101d31cf26e36fbc0b11dd3f8dd9113
svyatoslav-okshin <svyatokshin@gmail.com>
2020-05-05 20:15:32 -0400

    set up structure for viewportReducer.js and viewportActions.js

--- src/redux/actions/viewportActions.js
+++ src/redux/actions/viewportActions.js
@@ -1,1 +1,22 @@
 // todo
+import ReactGA from "react-ga";
+import * as types from "./actionTypes";
+
+
+export function setViewport() {
+    return (dispatch, getState) => {
+
+        dispatch({ type: types.SET_VIEWPORT});
+
+     
+    
+    }
+}
+
+export function setCityMarkers() {
+    return (dispatch, getState) => {
+        dispatch({ type: types.SET_CITYMARKERS});
+
+        
+    }
+}
--- src/redux/reducers/viewportReducer.js
+++ src/redux/reducers/viewportReducer.js
@@ -1,1 +1,30 @@
 // todo
+import * as types from "../actions/actionTypes";
+import citiesIndex from "../../data/city_ids.json";
+
+let cityIndex = [];
+Object.keys(citiesIndex).forEach((item) => {
+  let city = citiesIndex[item];
+  city.name = item;
+  cityIndex.push(city);
+});
+
+export const initialState = {
+    cityMarkers: cityIndex,
+    viewport: {
+        width: "100%",
+        height: "100%",
+        longitude: -96.7,
+        latitude: 38.55,
+        zoom: 3.55,
+        minZoom: 3.5,
+        maxZoom: 10,
+        trackResize: true,
+    },
+  };
+
+export default function viewportReducer(state = initialState, action) {
+    switch (action.type) {
+
+    }
+}
